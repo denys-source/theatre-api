74b009366660fae0d01a06f472e9ae85be5f0910
Denys <deibukdenyscloud@gmail.com>
2023-10-21 11:38:25 +0300

    Create new project, theatre and user apps

---
+++ config/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.conf import settings
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+] + (
+    [path("__debug__/", include("debug_toolbar.urls"))]
+    if settings.DEBUG
+    else []
+)
