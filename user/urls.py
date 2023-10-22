c5a41d3b1cb46d323267a14d3a9990cb3e98bf17
Denys <deibukdenyscloud@gmail.com>
2023-10-22 22:51:20 +0300

    Add user detail endpoint

--- user/urls.py
+++ user/urls.py
@@ -5,11 +5,12 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from user.views import UserCreateView
+from user.views import UserCreateView, UserDetailView
 
 
 urlpatterns = [
     path("register/", UserCreateView.as_view(), name="register"),
+    path("me/", UserDetailView.as_view(), name="manage"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
--- user/views.py
+++ user/views.py
@@ -1,9 +1,16 @@
-from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 
 from user.serializers import UserSerializer
 
 
 class UserCreateView(CreateAPIView):
-    queryset = get_user_model()
     serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
